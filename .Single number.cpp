d5d2c9782bb612ac75215adc8ed84dde31103947
Sakthi <125258052+sakthii21@users.noreply.github.com>
2025-06-08 21:15:58 +0530

    Add files via upload

---
+++ .Single number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+         int n =  nums.size();
+        //  int ans;
+        //  map<long long,int>mpp;
+        //  for(int i=0;i<n;i++){
+        //     mpp[nums[i]]++;
+        //  }
+        //  for(auto it:mpp){
+        //     if(it.second==1){
+        //         ans = it.first;
+        //     }
+        //  }
+        //  return ans;
+        int ans =0;
+        for(int i=0;i<n;i++){
+            ans = ans^nums[i];
+        }
+        return ans;
+    }
+
+};
