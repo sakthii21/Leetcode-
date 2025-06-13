522eb9c7eb0c20937073a6c33dd2371f2bf4467c
Sakthi <125258052+sakthii21@users.noreply.github.com>
2025-06-13 12:44:10 +0530

    Add files via upload

---
+++ Department Highest salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select  dept.name as Department , emp.name as Employee , emp.salary as Salary
+from Employee emp
+join Department dept on emp.departmentId = dept.id
+where emp.salary =(
+   select max(salary)
+   from Employee
+   where departmentId = emp.departmentId
+) 
+
